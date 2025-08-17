e83c7f91b10112cecb95ae0f6ab8436a39a1a3c3
Ravi <r2a3v5i7.kumar@gmail.com>
2025-08-17 13:17:22 -0700

    test

--- flake.nix
+++ flake.nix
@@ -9,11 +9,12 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
         [ 
+          pkgs.mkalias
           pkgs.neovim
           pkgs.tmux
           pkgs.rectangle
@@ -27,12 +28,33 @@
           pkgs.zoxide
           pkgs.git
           pkgs.gh
+          pkgs.alacritty
         ];
 
       fonts.packages =
       [
         pkgs.nerd-fonts.jetbrains-mono
       ];
+
+      system.activationScripts.applications.text = let
+  env = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = "/Applications";
+  };
+in
+  pkgs.lib.mkForce ''
+  # Set up applications.
+  echo "setting up /Applications..." >&2
+  rm -rf /Applications/Nix\ Apps
+  mkdir -p /Applications/Nix\ Apps
+  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+  while read -r src; do
+    app_name=$(basename "$src")
+    echo "copying $src" >&2
+    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+  done
+      '';
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
